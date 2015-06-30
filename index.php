f8bb64ecfc559ece8ffbb6cf913987afa313d8f6
avm99963 <jocdeladria@gmail.com>
2015-06-30 20:15:52 +0200

    Onecard design finished and login page developed

--- index.php
+++ index.php
@@ -3,9 +3,11 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>QuizMe</title>
+		<script src="bower_components/webcomponentsjs/webcomponents-lite.min.js"></script>
+		<link rel="import" href="custom_components/quizme-head/quizme-head">
+		<link rel="import" href="custom_components/quizme-login/quizme-login.html">
 	</head>
-	<body>
-		<header>
-		</header>
+	<body class="fullbleed" unresolved>
+		<quizme-login appname="QuizMe"></quizme-login>
 	</body>
 </html>
