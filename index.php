a17317069f5b4240775f93e58dd2950dc8f0b92c
avm99963 <jocdeladria@gmail.com>
2015-07-03 22:16:55 +0200

    Install script almost finished (doesn't yet create tables and add the first admin user)

--- index.php
+++ index.php
@@ -9,6 +9,28 @@
 		<link rel="import" href="custom_components/quizme-login/quizme-login.html">
 	</head>
 	<body class="fullbleed" unresolved>
+		<?php
+		if (!file_exists("config.php")) {
+		?>
+		<quizme-onecard>
+			<div class="content">
+				<div class="centered">
+					<h2>config.php file doesn't exist</h2>
+					<p>Please <a href="install.php">install QuizMe</a> first.</p>
+				</div>
+				<style>
+				.centered {
+					text-align: center;
+				}
+				</style>
+			</div>
+		</quizme-onecard>
+		<?php
+		} else {
+		?>
 		<quizme-login appname="QuizMe"></quizme-login>
+		<?php
+		}
+		?>
 	</body>
 </html>
