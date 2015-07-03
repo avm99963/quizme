ae1bdfc03338954e353724a1fa19af5a0ace6810
avm99963 <jocdeladria@gmail.com>
2015-07-03 12:18:22 +0200

    Installer

--- index.php
+++ index.php
@@ -3,8 +3,9 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>QuizMe</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<script src="bower_components/webcomponentsjs/webcomponents-lite.min.js"></script>
-		<link rel="import" href="custom_components/quizme-head/quizme-head">
+		<link rel="import" href="custom_components/quizme-head/quizme-head.html">
 		<link rel="import" href="custom_components/quizme-login/quizme-login.html">
 	</head>
 	<body class="fullbleed" unresolved>
